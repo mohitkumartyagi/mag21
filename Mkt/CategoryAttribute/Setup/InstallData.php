ffb3a33cd0c2da1f43c6ac8c04b992ca816fe527
mohit tyagi <mohitime2007@gmail.com>
2018-06-21 00:18:27 +0530

    commit to master
    commit to master

---
+++ Mkt/CategoryAttribute/Setup/InstallData.php
@@ -0,0 +1,51 @@
+<?php
+/* file: app/code/Mkt/CategoryAttribute/Setup/InstallData.php */
+
+namespace Mkt\CategoryAttribute\Setup;
+
+use Magento\Framework\Setup\InstallDataInterface;
+use Magento\Framework\Setup\ModuleContextInterface;
+use Magento\Framework\Setup\ModuleDataSetupInterface;
+use Magento\Eav\Setup\EavSetup;
+use Magento\Eav\Setup\EavSetupFactory;
+use Magento\Catalog\Model\Category;
+use Magento\Eav\Model\Entity\Attribute\ScopedAttributeInterface;
+
+/**
+ * @codeCoverageIgnore
+ */
+class InstallData implements InstallDataInterface
+{
+    /**
+     * @var EavSetupFactory
+     */
+    private $eavSetupFactory;
+
+    /**
+     *
+     * @param EavSetupFactory $eavSetupFactory
+     */
+    public function __construct(EavSetupFactory $eavSetupFactory)
+    {
+        $this->eavSetupFactory = $eavSetupFactory;
+    }
+
+    public function install(ModuleDataSetupInterface $setup, ModuleContextInterface $context)
+    {
+        /** @var EavSetup $eavSetup */
+        $eavSetup = $this->eavSetupFactory->create(['setup' => $setup]);
+        $eavSetup->addAttribute(
+            Category::ENTITY,
+            'category_sku',
+            [
+                'type' => 'varchar',
+                'label' => 'Category Code',
+                'input' => 'text',
+                'required' => false,
+                'sort_order' => 100,
+                'global' => \Magento\Eav\Model\Entity\Attribute\ScopedAttributeInterface::SCOPE_STORE,
+                'group' => 'General Information',
+            ]
+        );
+    }
+}
---
+++ Mkt/CategoryAttribute/registration.php
@@ -0,0 +1,7 @@
+<?php
+/* file: app/code/Mkt/CategoryAttribute/registration.php  */
+\Magento\Framework\Component\ComponentRegistrar::register(
+    \Magento\Framework\Component\ComponentRegistrar::MODULE,
+    'Mkt_CategoryAttribute',
+    __DIR__
+);
